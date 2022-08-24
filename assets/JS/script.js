4b7c286eab0b1df996a5a4905f9a0628da254226
toghrulvg <toghrulvg@code.edu.az>
2022-08-24 06:51:34 +0400

    added

---
+++ assets/JS/script.js
@@ -0,0 +1,126 @@
+// let text =
+//     "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+
+//   let result = text.substring(0,65) + " ...";
+//   console.log(result);
+
+// let num = 36;
+
+// console.log(num.toString());
+// console.log(country.length);
+
+// for (const iterator of country) {
+//   console.log(iterator);
+// }
+
+// console.log(country[1]);
+
+// console.log(country.toLocaleUpperCase());
+
+// console.log(country.slice(4));
+
+// console.log(country.substring(0,7));
+
+// let country = "Azerbaycan";
+// let addStr = " Respublikasi"
+
+// console.log(country.substring(1,5));
+// console.log(country.replace("bay", "Saol"));
+// console.log(country.concat(addStr));
+// console.log(addStr.trim());
+// console.log(country.startsWith("A"));
+
+// if (country.startsWith("A")) {
+//     console.log("Yes");
+// }else{
+//     console.log("No");
+// }
+
+// console.log(country.charAt(3));
+// console.log(country.charCodeAt(0));
+
+// let result = country.split("-");
+// console.log(result);
+
+
+// console.log(country.indexOf("Azerbaycan"));
+
+// function search(str){
+//     if (country.indexOf(str) == -1) {
+//         console.log("Tapilmadi");
+//         return;
+//     }
+//     console.log("Tapildi");
+// }
+
+// search("test");
+
+
+// let city = "Baki";
+// console.log(city.includes("b"));
+
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number("10"));
+// console.log(Number("Jhon"));
+
+// Number(true);
+// Number(false);
+// Number("10");
+// Number("  10");
+// Number("10  ");
+// Number(" 10  ");
+// Number("10.33");
+// Number("10.33");
+// Number("10.33");
+// Number("Jhon");
+
+// let count = "sasasa;
+// console.log(typeof count);
+
+
+
+// let result = int.parse(count);
+// console.log(typeof result);
+
+// let users = [ "Togrul","Cavid",  "Seadet", "Fidan","Ayxan"];
+// console.log(users.toString());
+// console.log(users.join(" "));
+// users.push("Emil")
+
+// users.pop();
+
+// users.unshift("hello");
+
+// users.unshift(2);
+
+// console.log(users);
+
+// const myGirls = ["Saadat", "Fidan"];
+// const myBoys = ["Togrul", "Cavid", "Ayxan"]
+
+// const myChildren = myGirls.concat(myBoys);
+
+// console.log(myChildren);
+
+// users.splice(3,2,"Ilkin");
+
+
+// let users = [ "Togrul","Cavid",  "Seadet", "Fidan","Ayxan"];
+// console.log(users);
+
+// let result = users.slice(2,2,"Seid");
+// console.log(users);
+
+// const fruits = [ "Lemon", "Apple", "Banana", "Orange", "Mango"];
+// const citrus = fruits.slice(1, 3);
+
+// console.log(citrus);
+
+// users.sort();
+// console.log(users);
+
+// const points = [40, 100, 1, 5, 25, 10];
+// console.log(points);
+// points.sort(function(a, b){return a - b});
+// console.log(points);
